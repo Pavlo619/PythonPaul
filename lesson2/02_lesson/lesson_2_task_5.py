c968adec59e3ea6c98db0c6bf4a208b8e6b3ff3f
Pavlo619 <diakonov.pavel2011@gmail.com>
2025-04-20 17:47:59 +0300

    Merge remote-tracking branch 'origin/lesson1' into lesson1
    Signed-off-by: Pavlo619 <diakonov.pavel2011@gmail.com>

--- lesson2/02_lesson/lesson_2_task_5.py
+++ lesson2/02_lesson/lesson_2_task_5.py
@@ -5,7 +5,7 @@ def month_to_season(month):
         return "Лето"
     elif 9 <= month <= 11:
         return "Осень"
-    elif month == 11 or month == 1 or month == 2:
+    elif month == 12 or month == 1 or month == 2:
         return "Зима"
     else:
         return "Неверный номер месяца"
